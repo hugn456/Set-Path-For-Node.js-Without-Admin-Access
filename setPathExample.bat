72eaf252df3174ac75aa9a3a6e8a9ef45487e068
Huy Gia Nguyen <90328373+hugn456@users.noreply.github.com>
2022-07-05 18:24:53 +1200

    Add files via upload

---
+++ setPathExample.bat
@@ -0,0 +1,8 @@
+:: move to the directory which is storing this file setPath.bat 
+@cd C:\Users\hugn456\Downloads\node-v16.15.1-win-x64\node-v16.15.1-win-x64
+
+:: set the path for the binary Node.js 
+@set PATH=C:\Users\hugn456\Downloads\node-v16.15.1-win-x64\node-v16.15.1-win-x64;%PATH%
+
+:: save the property of this file as cmd.exe
+@cmd.exe /K
